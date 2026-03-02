b25e8f32807f389026fad51d3570d3f7450c1958
Richard Mora <richard.mora0511@gmail.com>
2026-03-01 19:59:53 -0800

    completed the remove from array exercise, with the help from the solution folder, for The Odin Project according to the readme file

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!num.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;  
 };
 
 // Do not edit below this line
