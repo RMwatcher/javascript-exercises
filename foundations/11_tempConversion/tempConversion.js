8b579724da19d3a1df7ceadef00a352daabb2390
Richard Mora <richard.mora0511@gmail.com>
2026-03-02 22:06:02 -0800

    completed the exercise tempConversion from The Odin Project where my answers are different from the solution file based on the readme file.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp, decimalPlaces = 1) {
+  const factor = Math.pow(10, decimalPlaces);
+  let convertedTemp = (temp - 32) * 5/9;
+  return Math.round(convertedTemp * factor) / factor;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp, decimalPlaces = 1) {
+  const factor = Math.pow(10, decimalPlaces);
+  let convertedTemp = (temp * 9/5 + 32);
+  return Math.round(convertedTemp * factor) / factor;
 };
 
 // Do not edit below this line
