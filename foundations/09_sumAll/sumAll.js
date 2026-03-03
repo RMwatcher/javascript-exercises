c85f6262d942f21a88d6eb33da75b1daa23b4c25
Richard Mora <richard.mora0511@gmail.com>
2026-03-02 20:48:04 -0800

    completed sumAll execise from The Odin Project based on the readme file. Use different method from the solution folder.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let newArray = [];
+    let result = "";
+    if (first < 0 || second < 0 || typeof (frist) == 'string' || typeof (second) == 'string' || Array.isArray(first) || Array.isArray(second) || first % 1 != 0 || second % 1 != 0) return "ERROR";
+    
+    if (first < second) {
+        for (let i = first; i <= second; i++) {
+            newArray.push(i);
+        }
+    } else {
+        for (let i = second; i <= first; i++) {
+            newArray.push(i);
+        }
+    }
+    
+    return result = newArray.reduce((sum, current) => sum + current, 0);
 };
 
 // Do not edit below this line
