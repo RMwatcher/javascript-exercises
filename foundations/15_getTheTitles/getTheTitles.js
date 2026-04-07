0e5b3b52af2cf28004cdaee8016085262f62aecf
Richard Mora <richard.mora0511@gmail.com>
2026-04-06 23:56:18 -0700

    completed exercise getTheTitles for The Odin Project

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let booksTitle = array.map(book => book.title);
+    return booksTitle;
 };
 
 // Do not edit below this line
