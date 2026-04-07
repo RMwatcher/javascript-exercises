1462a31418e140f67dbde8af2e045c7efc05b8c8
Richard Mora <richard.mora0511@gmail.com>
2026-04-06 23:47:11 -0700

    completed exercise fibonacci for The Odin Project

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+    let sequence;
+    if (typeof number !== "number") {
+        sequence = parseInt(number);
+    } else {
+        sequence = number;
+    }
 
+    if (sequence < 0) {
+        return "OOPS";
+    } else if (sequence == 0 ) {
+        return 0;
+    }
+
+    let first = 1;
+    let second = 0;
+
+    for (let i = 2; i <= sequence; i++) {
+        let current = first + second;
+        second = first;
+        first = current;
+    }
+
+    return first;
 };
 
 // Do not edit below this line
