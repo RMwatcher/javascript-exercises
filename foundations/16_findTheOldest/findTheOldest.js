69270ed45da13c16974eacb339c1bc1f9f041bdd
Richard Mora <richard.mora0511@gmail.com>
2026-04-13 23:09:57 -0700

    completed exercise 16 findTheOldest from The Odin Project

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let yearAdded = new Date();
 
-};
+    array.forEach(element => {
+        if (!(element.yearOfDeath)) {
+        element.yearOfDeath = yearAdded.getFullYear();
+    }  
+    });
+    
+    return sortByAge = array.reduce((a, b) => (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? b : a);
+    
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
